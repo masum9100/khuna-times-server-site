fba4468b8cb8ab97c22e4e15d2c7af5bd4a3e33c
masum9100 <masum.seopro@gmail.com>
2023-12-01 17:15:20 +0600

    User delete and make admin done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const jwt = require('jsonwebtoken');
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5001
@@ -15,115 +16,144 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-const newsCollection = client.db('khulnaToday').collection('news')
-const newArticleCollection = client.db('khulnaToday').collection('newarticle')
-const userCollection = client.db('khulnaToday').collection('users')
-
-
-
-// User Collection 
-
-app.post('/users', async(req, res)=>{
-    const user = req.body
-    const result = await userCollection.insertOne(user)
-    res.send(result)
-})
-
-// News collection 
+        const newsCollection = client.db('khulnaToday').collection('news')
+        const newArticleCollection = client.db('khulnaToday').collection('newarticle')
+        const userCollection = client.db('khulnaToday').collection('users')
 
 
-app.get('/news', async(req, res)=>{
-    const result = await newsCollection.find().toArray()
-    res.send(result)
-})
 
-app.get('/news/:id', async(req, res)=>{
-    const id  = req.params.id
-    const query = {_id: new ObjectId(id)}
-    const result = await newsCollection.findOne(query)
-    res.send(result)
-})
+        // User Collection 
 
-// new article 
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray()
+            res.send(result)
+        })
 
-app.get('/newarticle', async(req, res)=>{
-    
-    let query ={}
-    if (req.query?.user_email){
-        query = {user_email: req.query.user_email}
+        app.post('/users', async (req, res) => {
+            const user = req.body
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query)
+            if (existingUser) {
+                return res.send({ message: 'user already exist', insertedId: null })
+            }
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc)
+            res.send(result)
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        // News collection 
+
+
+        app.get('/news', async (req, res) => {
+            const result = await newsCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/news/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await newsCollection.findOne(query)
+            res.send(result)
+        })
+
+        // new article 
+
+        app.get('/newarticle', async (req, res) => {
+
+            let query = {}
+            if (req.query?.user_email) {
+                query = { user_email: req.query.user_email }
+            }
+            const result = await newArticleCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.get('/newarticle/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await newArticleCollection.findOne(query)
+            res.send(result)
+        })
+
+        app.post('/newarticle', async (req, res) => {
+            const newArticle = req.body
+            const result = await newArticleCollection.insertOne(newArticle)
+            res.send(result)
+        })
+
+        app.put('/newarticle/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true }
+            const updateArticle = req.body
+            const article = {
+                $set: {
+
+                    newsTitle: updateArticle.newsTitle,
+                    short_description: updateArticle.short_description,
+                    long_description: updateArticle.long_description,
+                    photo_url1: updateArticle.photo_url1,
+                    photo_url2: updateArticle.photo_url2,
+                    user_email: updateArticle.user_email,
+                    tag: updateArticle.tag,
+
+                }
+            }
+            const result = await newArticleCollection.updateOne(filter, article, options)
+            res.send(result)
+        })
+
+        app.delete('/newarticle/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await newArticleCollection.deleteOne(query)
+            res.send(result)
+        })
+
+
+
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
     }
-    const result = await newArticleCollection.find(query).toArray()
-    res.send(result)
-})
-
-app.get('/newarticle/:id', async(req, res)=>{
-    const id = req.params.id
-    const query = {_id: new ObjectId(id)}
-    const result = await newArticleCollection.findOne(query)
-    res.send(result)
-})
-
-app.post('/newarticle', async(req, res) =>{
-    const newArticle = req.body
-    const result = await newArticleCollection.insertOne(newArticle)
-    res.send(result)
-})
-
-app.put('/newarticle/:id', async(req, res) =>{
-    const id = req.params.id
-    const filter = {_id: new ObjectId(id)}
-    const options = {upsert: true}
-    const updateArticle = req.body
-    const article ={
-        $set: {
-            
-            newsTitle: updateArticle.newsTitle,
-            short_description: updateArticle.short_description,
-            long_description: updateArticle.long_description,
-            photo_url1: updateArticle.photo_url1,
-            photo_url2: updateArticle.photo_url2,
-            user_email: updateArticle.user_email,
-            tag: updateArticle.tag,
-          
-        }
-    }
-    const result = await newArticleCollection.updateOne(filter, article, options)
-    res.send(result)
-})
-
-app.delete('/newarticle/:id', async(req, res) =>{
-    const id = req.params.id
-    const query = {_id: new ObjectId(id)}
-    const result = await newArticleCollection.deleteOne(query)
-    res.send(result)
-})
-
-
-
-
-
-
-
-    
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
 }
 run().catch(console.dir);
 
